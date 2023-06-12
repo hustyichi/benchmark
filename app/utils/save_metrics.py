1b2dcf8ec46cbc24c06a5420236ae845850d55c3
libing <libing@healink.cn>
2023-06-12 11:06:25 +0800

    MOD: update usage

--- app/utils/save_metrics.py
+++ app/utils/save_metrics.py
@@ -6,13 +6,14 @@ class FileMetrics(object):
         columns = ["memory_usage", "cpu_usage"]
         with open(self.file_path, "w") as file:
             raw_data = ",".join(columns)
-            file.writelines([raw_data,])
+            file.write(f"{raw_data}\n")
 
     def append_metric(self, metric_msg: str):
         with open(self.file_path, "a") as file:
-            file.writelines([metric_msg, ])
+            file.write(f"{metric_msg}\n")
 
     def append_monitor_metric(self, memory_usage: int, cpu_usage: float):
-        metric = [memory_usage, cpu_usage]
+        metric = [str(memory_usage), str(cpu_usage)]
+        print(f"Memory usage: {memory_usage}, cpu usage percent {cpu_usage}")
         raw_metric = ",".join(metric)
         self.append_metric(raw_metric)
